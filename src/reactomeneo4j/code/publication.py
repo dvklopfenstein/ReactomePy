4dc9a524879f34db6437319c0e42b6ec40004905
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2018-10-30 23:20:34 -0400

    Report pathway Summary and publications.

--- src/reactomeneo4j/code/publication.py
+++ src/reactomeneo4j/code/publication.py
@@ -12,6 +12,7 @@ __author__ = "DV Klopfenstein"
 
 import sys
 from importlib import import_module
+import textwrap
 
 class Publication(object):
     """Manages Publications: Research papers, books, and URLs."""
@@ -21,8 +22,9 @@ class Publication(object):
     PYPM = 'reactomeneo4j.data.{ABC}.pathways.pwy2pmids'
     PMDS = 'reactomeneo4j.data.{ABC}.pathways.pmid2nt'
 
-    def __init__(self, abc):
+    def __init__(self, abc, linewidth=80):
         self.abc = abc
+        self.linewidth = linewidth
         self.pw2nt = {nt.stId:nt for nt in import_module(self.PWYS.format(ABC=abc)).PWYNTS}
         self.pw2sum = import_module(self.SUMS.format(ABC=abc)).PW2SUMS
         self.pw2pmids = import_module(self.PYPM.format(ABC=abc)).PWY2PMIDS
@@ -32,7 +34,14 @@ class Publication(object):
 
     def prt_pw(self, pwy_stid, prt=sys.stdout):
         """Print Pathway information."""
-        prt.write('{NT}\n'.format(NT=self.pw2nt[pwy_stid]))
+        prt.write('\nPATHWAY: {stId} {releaseDate} {marks} {displayName}\n'.format(**(self.pw2nt[pwy_stid])._asdict()))
+        for idx, summary in enumerate(self.pw2sum[pwy_stid]):
+            prt.write('SUMMARY: {TXT}\n'.format(TXT=('\n'.join(textwrap.wrap(summary, self.linewidth)))))
+        if pwy_stid in self.pw2pmids:
+            pmid_nt = sorted([(p, self.pmid2nt[p]) for p in self.pw2pmids[pwy_stid]], key=lambda t: -1*t[1].year)
+            for pmid, ntpub in pmid_nt:
+                prt.write('\nPMID: {year} {PMID:8} {journal}\n'.format(PMID=pmid, year=ntpub.year, journal=ntpub.journal))
+                prt.write('{displayName}\n'.format(displayName=ntpub.displayName))
 
     def get_pwys_w_all(self):
         """Get a set of Pathways that contain all types of descriptions."""
