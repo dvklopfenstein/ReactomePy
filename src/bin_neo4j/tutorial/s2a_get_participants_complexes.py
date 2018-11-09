d08eef629c292a1064d751e01c5cd65654148ede
DV Klopfenstein <dvklopfenstein@gmail.com>
2018-11-09 16:08:59 -0500

    Moved non-tutorial files to run dir

--- src/bin_neo4j/tutorial/s2a_get_participants_complexes.py
+++ src/bin_neo4j/tutorial/s2a_get_participants_complexes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-"""Mirror Reactome/Neo4j tutorial in Python."""
-# https://reactome.org/dev/graph-database/extract-participating-molecules
+"""Breaking down complexes and sets to get thier participants."""
+# https://reactome.org/dev/graph-database/extract-participating-molecules#complexes-sets-participants
 
 from __future__ import print_function
 
@@ -11,31 +11,29 @@ import sys
 from neo4j import GraphDatabase
 
 
-def main(password, abc='hsa'):
-    """Mirror Reactome/Neo4j tutorial in Python."""
+# MATCH (Complex{stId:"R-HSA-983126"})-[:hasComponent]->(pe:PhysicalEntity) RETURN pe.stId AS component_stId, pe.displayName AS component
+def main(password):
+    """Breaking down complexes and sets to get thier participants."""
     gdbdr = GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', password))
     with gdbdr.session() as session:
-        _run(session)
+        #     component_stId   component
+        #     --------------   ----------
+        #     'R-HSA-976075    E3 ligases in proteasomal degradation [cytosol]
+        #     'R-ALL-983035    antigenic substrate [cytosol]
+        #     'R-HSA-976165    Ubiquitin:E2 conjugating enzymes [cytosol]
+        qry = ('MATCH (Complex{stId:"R-HSA-983126"})-[:hasComponent]->(pe:PhysicalEntity) RETURN '
+               'pe.stId AS component_stId, pe.displayName AS component')
+        for dct in session.run(qry).data():
+            print('{component_stId} {component}'.format(**dct))
 
-
-def _run(session):
-    """2) BREAKING DOWN COMPLEXES AND SETS TO GET THEIR PARTICIPANTS"""
-    # https://reactome.org/content/schema/Complex
-    # MATCH (Complex{stId:"R-HSA-983126"})-[:hasComponent]->(pe:PhysicalEntity) RETURN pe.stId AS component_stId, pe.displayName AS component
-    qry = ('MATCH (Complex{stId:"R-HSA-983126"})-'
-           '[:hasComponent]->(pe:PhysicalEntity) RETURN '
-           'pe.stId AS component_stId, '
-           'pe.displayName AS component')
-    
-    #     component_stId   component
-    #     --------------   ----------
-    #     'R-HSA-976075    E3 ligases in proteasomal degradation [cytosol]
-    #     'R-ALL-983035    antigenic substrate [cytosol]
-    #     'R-HSA-976165    Ubiquitin:E2 conjugating enzymes [cytosol]
-    for dct in session.run(qry).data():
-        print('{ID} {NAME}'.format(ID=dct['component_stId'], NAME=dct['component']))
-
-
+        qry = ('MATCH (Complex{stId:"R-HSA-983126"})'
+               '-[:hasComponent|hasMember|hasCandidate*]->(pe:PhysicalEntity) RETURN '
+               'pe.stId AS component_stId, pe.displayName AS component')
+        fout_txt = 'complex_components_all.txt'
+        with open(fout_txt, 'w') as prt:
+            for idx, dct in enumerate(session.run(qry).data()):
+                prt.write('{component_stId} {component}\n'.format(**dct))
+            print('  {N} compents WROTE: {TXT}'.format(N=idx, TXT=fout_txt))
 
 if __name__ == '__main__':
     assert len(sys.argv) != 1, 'First arg must be your Neo4j database password'
