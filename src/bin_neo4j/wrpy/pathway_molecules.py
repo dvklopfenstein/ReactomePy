d4af38560ff8efefbc40fdc1d0929c0de0b17486
DV Klopfenstein <dvklopfenstein@gmail.com>
2018-11-11 23:35:49 -0500

    Print ALL proteins in each pathway for human, mouse, fly

--- src/bin_neo4j/wrpy/pathway_molecules.py
+++ src/bin_neo4j/wrpy/pathway_molecules.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
-"""Write pathway2proteins into a Python module.
-
-Usage: pathway_molecules.py <neo4j_password> [--schemaClass=CLS] [--species=S] [-o] [-r]
-
-Options:
-  -h --help
-  -c --schemaClass=CLS  Examples: Complex, Pathway, etc. [default: Complex]
-  -s --species=S        Species [default: Homo sapiens]
-  -o                    Write results into a file rather than to the screen
-  -r                    Report lower-level source schema
-
-"""
-# https://reactome.org/dev/graph-database/extract-participating-molecules#joining-pieces
+"""Write pathway2proteins into a Python module."""
 
 from __future__ import print_function
 
@@ -19,22 +7,28 @@ __copyright__ = "Copyright (C) 2018-2019, DV Klopfenstein. All rights reserved."
 __author__ = "DV Klopfenstein"
 
 import sys
-import collections as cx
-from docopt import docopt
-from neo4j import GraphDatabase
 from reactomeneo4j.code.wrpy.pathway_molecules import PathwayMolecules
-from reactomeneo4j.data.species import SPECIES
+
 
 # pylint: disable=line-too-long
 def main(password, schemaname='Complex', stid='R-HSA-983126', prt=sys.stdout):
     """Save the participating molecules for a pathway into a Python module."""
 
+    # 2049 Pathways contain 10912 items from UniProt for Homo sapiens
+    # 1623 Pathways contain 12207 items from UniProt for Mus musculus
+    # 1374 Pathways contain  9316 items from UniProt for Drosophila melanogaster
+    #     ->
+    # src/reactomeneo4j/data/hsa/pathways/pwy2uniprot.py
+    # src/reactomeneo4j/data/dme/pathways/pwy2uniprot.py
+    # src/reactomeneo4j/data/mmu/pathways/pwy2uniprot.py
+    species = [
+        'Homo sapiens',
+        'Mus musculus',
+        'Drosophila melanogaster',
+    ]
     obj = PathwayMolecules(password)
-    data = obj.get_pw2molecules('Homo sapiens', 'UniProt')
-    #### # _prt_data(item_id, data, prt)
-    #### with open(fout_txt, 'w') as prt:
-    ####     _prt_data(data, prt)
-    ####     print('  {N} WROTE: {TXT}'.format(N=len(data), TXT=fout_txt))
+    for org in species:
+        obj.wrpy_pw2molecules(org, 'UniProt')
 
 
 if __name__ == '__main__':
--- src/reactomeneo4j/code/wrpy/pathway_molecules.py
+++ src/reactomeneo4j/code/wrpy/pathway_molecules.py
@@ -5,10 +5,17 @@ from __future__ import print_function
 __copyright__ = "Copyright (C) 2018-2019, DV Klopfenstein. All rights reserved."
 __author__ = "DV Klopfenstein"
 
+import os
 import sys
 import collections as cx
 from neo4j import GraphDatabase
 from reactomeneo4j.data.species import SPECIES
+from reactomeneo4j.code.wrpy.utils import REPO
+from reactomeneo4j.code.wrpy.utils import prt_docstr_module
+from reactomeneo4j.code.wrpy.utils import prt_namedtuple
+from reactomeneo4j.code.wrpy.utils import prt_dict
+from reactomeneo4j.code.wrpy.utils import prt_copyright_comment
+
 
 # pylint: disable=line-too-long
 class PathwayMolecules(object):
@@ -16,7 +23,7 @@ class PathwayMolecules(object):
 
     def __init__(self, password):
         self.gdbdr = GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', password))
-        self.name2ntabc = {nt.taxId:nt for nt in SPECIES}
+        self.name2ntabc = {nt.displayName:nt for nt in SPECIES}
 
     def get_query(self, species, database):
         """Return all paticipating molecules for all pathways in a species."""
@@ -56,19 +63,38 @@ class PathwayMolecules(object):
             query = self.get_query(species, database)
             # print("QUERY: {Q}".format(Q=query))
             for rec in session.run(query).records():
-                print(rec)
+                # print(rec)
                 pwid2molecules[rec['pwid']].add(rec['mol_id'])
         return pwid2molecules
 
-def _prt_data(data, prt):
-    """Print the Participating molecules for a pathway."""
-    msg = '{N} participants in Pathway(R-HSA-983169)'.format(N=len(data))
-    prt.write("{MSG}\n".format(MSG=msg))
-    prt.write('Identifier Database\n')
-    prt.write('---------- --------\n')
-    for dct in sorted(data, key=lambda d: [d['Database'], d['Identifier']]):
-        prt.write('{Identifier:10} {Database}\n'.format(**dct))
-    print(msg)
+    def wrpy_pw2molecules(self, species='Homo sapiens', database='UniProt'):
+        """Print the Participating molecules for a pathway."""
+        fout_py = "src/reactomeneo4j/data/{ABC}/pathways/pwy2{DB}.py".format(
+            ABC=self.name2ntabc[species].abc, DB=database.lower())
+        pw2molecules = self.get_pw2molecules(species, database)
+        molecules = set(m for ms in pw2molecules.values() for m in ms)
+        msg = '{N:4} Pathways contain {M:5} items from {DB} for {ORG:23}'.format(
+            N=len(pw2molecules), M=len(molecules), DB=database, ORG=species)
+        with open(os.path.join(REPO, fout_py), 'w') as prt:
+            prt_docstr_module(msg, prt)
+            prt.write('PWY2ITEMS = {\n')
+            for pwy, molecules in sorted(pw2molecules.items(), key=lambda t: int(t[0].split('-')[2])):
+                prt.write("    '{PWY}':".format(PWY=pwy))
+                mstrs = ["'{V}'".format(V=m) for m in sorted(molecules)]
+                prt.write("{{{SET}}},\n".format(SET=", ".join(mstrs)))
+            # prt_namedtuple(self.dcts, 'SPECIES', fields, prt)
+            prt.write('}\n')
+            prt_copyright_comment(prt)
+        print("  {MSG} WROTE: {PY}".format(MSG=msg, PY=fout_py))
+
+    def wrpy_info(self, fout_py):
+        """Print Reactome species main information."""
+        fields = ['abc', 'abbreviation', 'taxId', 'displayName']
+        with open(os.path.join(REPO, fout_py), 'w') as prt:
+            prt_docstr_module('Species in Reactome', prt)
+            prt_namedtuple(self.dcts, 'SPECIES', fields, prt)
+            prt_copyright_comment(prt)
+            print('  WROTE: {PY}'.format(PY=fout_py))
 
 
 # Copyright (C) 2018-2019, DV Klopfenstein. All rights reserved.
