50ebba2e6d29fec90380f0ae052564bac301c811
DV Klopfenstein <dvklopfenstein@users.noreply.github.com>
2018-12-23 21:13:38 -0500

    Moved get_hms(tic) to utils

--- src/reactomeneo4j/code/utils.py
+++ src/reactomeneo4j/code/utils.py
@@ -3,7 +3,10 @@
 __copyright__ = "Copyright (C) 2018-2019, DV Klopfenstein. All rights reserved."
 __author__ = "DV Klopfenstein"
 
+import timeit
+import datetime
 import collections as cx
+from docopt import docopt
 
 
 def chk_unique(dcts, fld2expunique):
@@ -17,5 +20,24 @@ def chk_unique(dcts, fld2expunique):
         actually_unique = len(items) == num_dcts
         assert actually_unique == fld2expunique[fld]
 
+def get_args(docstr, fields):
+    """Given a doc string and desired fields, return a namedtuple w/user values."""
+    fld2docopt = {
+        'url': '--url',
+        'neo4j_username': '--neo4j_username',
+        'neo4j_password': '<neo4j_password>',
+    }
+    args = docopt(docstr)
+    dct = {}
+    for usrfld in fields:
+        argfld = fld2docopt[usrfld]
+        argval = args[argfld]
+        dct[usrfld] = argval
+    ntobj = cx.namedtuple('NtArgs', [f for f in fields if f in fld2docopt])
+    return ntobj(**dct)
+
+def get_hms(tic):
+    """Return HMS since script started."""
+    return str(datetime.timedelta(seconds=(timeit.default_timer()-tic)))
 
 # Copyright (C) 2018-2019, DV Klopfenstein. All rights reserved.
